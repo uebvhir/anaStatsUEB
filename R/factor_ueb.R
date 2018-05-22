d8518bf40b9c1790de77fcf8763682f1082d1664
miriammota <mmota.foix@gmail.com>
2018-05-22 16:19:56 +0200

    factor_ueb, millores

--- R/factor_ueb.R
+++ R/factor_ueb.R
@@ -14,7 +14,7 @@
 #' variable <- c(0,0,0,1,1,1,0,10,1,0)
 #' factor_ueb(variable, levels = c(0,1), labels = c("No", "Si"))
 #'
-#' variable <- c(0,0,0,1,1,1,0,1,1,0)
+#' variable <- c(0,0,0,1,1,1,0,1,1,0, NA)
 #' factor_ueb(variable, levels = c(0,1), labels = c("No", "Si"))
 #' @return factor returns an object of class "factor" which has a set of integer codes the length of x with a "levels" attribute of mode character and unique (!anyDuplicated(.)) entries. If argument ordered is true (or ordered() is used) the result has class c("ordered", "factor"). Undocumentedly for a long time, factor(x) loses all attributes(x) but "names", and resets "levels" and "class".
 
@@ -22,8 +22,10 @@
 
 
 factor_ueb <- function(x, levels, labels, del.empty.val = TRUE){
-  levels_dif <- unique(x) %in% levels
-  if (!all(levels_dif)) { warning("Los individuos con valor '", unique(x)[!levels_dif],
+
+  unique_noNA <- unique(x)[!unique(x) %in% NA]
+  levels_dif <- unique_noNA  %in% levels
+  if (!all(levels_dif)) { warning("Los individuos con valor '", unique_noNA[!levels_dif],
                                  "' han sido considerados NA \n", call. = FALSE) }
   var_factor <- factor(x, levels = levels, labels = labels )
   if (del.empty.val) var_factor <- factor(var_factor)
