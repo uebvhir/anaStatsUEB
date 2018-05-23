bc2bfe890cb3cf3bf4655082cccb7c8dc0978166
miriammota <mmota.foix@gmail.com>
2018-05-23 09:56:49 +0200

    posposar canvis

--- R/factor_ueb.R
+++ R/factor_ueb.R
@@ -5,7 +5,6 @@
 #' @param levels an optional vector of the values (as character strings) that x might have taken. The default is the unique set of values taken by as.character(x), sorted into increasing order of x. Note that this set can be specified as smaller than sort(unique(x))
 #' @param labels either an optional character vector of (unique) labels for the levels (in the same order as levels after removing those in exclude), or a character string of length 1.
 #' @param del.empty.val elimina los niveles que tienen 0 casos
-#' @param show.id TRUE o FALSE. Muestra codigo para ver que individuos que se han perdido. Por defecto, TRUE.
 #' @export factor_ueb
 #' @author Miriam Mota  \email{miriam.mota@@vhir.org}
 #' @examples
@@ -22,22 +21,19 @@
 #' @keywords factor variable class levels labels
 
 
-factor_ueb <- function(x, levels, labels, del.empty.val = TRUE, name.var = NULL, show.id = TRUE){
+factor_ueb <- function(x, levels, labels, del.empty.val = TRUE, name.var = NULL){
 
   name_sep <- strsplit(deparse(substitute(x)), "$",fixed = T)[[1]]
   name.var <- ifelse(is.null(name.var), name_sep[length(name_sep)], name.var)
   unique_noNA <- unique(x)[!unique(x) %in% NA]
   levels_dif <- unique_noNA  %in% levels
+  # paste0 ( unique_noNA[!levels_dif], which(x == unique_noNA[!levels_dif])   )
   if (!all(levels_dif)) { warning("Los individuos con valor '", paste(unique_noNA[!levels_dif], collapse = "', '"),
                                   "' para la variable ",name.var," han sido considerados NA \n", call. = FALSE) }
   var_factor <- factor(x, levels = levels, labels = labels )
   if (del.empty.val) var_factor <- factor(var_factor)
   if (show.id) {
-    message(paste0("
-Para ver que individuos son los que se han perdido,
-copiar el siguiente codigo cambiando id por la variable
-identificadora:
-dat$id[dat$",name.var," == '", unique_noNA[!levels_dif], "'] \n"))
+
     return(var_factor)
   }
 }
