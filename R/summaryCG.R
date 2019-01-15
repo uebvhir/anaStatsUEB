c25959a7786c7b06f2aebd3e95edb9097fc97967
miriammota <mmota.foix@gmail.com>
2019-01-15 12:37:55 +0100

    font size table

--- R/summaryCG.R
+++ R/summaryCG.R
@@ -107,8 +107,8 @@ summaryCG <- function(res,
     #       include.rownames = FALSE, tabular.environment = "longtable", floating = FALSE)
 
     resum <- kable(resum, format = xtab.type, booktabs = T,
-          caption = title, longtable = TRUE, escape = F)%>%
-      kable_styling(latex_options = c("striped","hold_position", "repeat_header"), sz.xtab = size, full_width = F, position = "left")
+          caption = title, longtable = TRUE, escape = F) %>%
+      kable_styling(latex_options = c("striped","hold_position", "repeat_header"), font_size = sz.xtab, full_width = F, position = "left")
   } else {
     return(resum)
   }
