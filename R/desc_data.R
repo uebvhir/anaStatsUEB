42014d1e6255759ea6d0900ff6568ea669f2b52f
miriammota <mmota.foix@gmail.com>
2019-04-09 11:59:22 +0200

    prova final?

--- R/desc_data.R
+++ R/desc_data.R
@@ -9,10 +9,10 @@
 #' @param caption Character vector containing the table's caption or title. Default value is a summary.
 #' @keywords read clean data summary depurate
 #' @export desc_data
-#' @import kableExtra papeR dplyr
+#' @import kableExtra Hmisc dplyr
 #' @examples
-#' desc_data(iris, format = "html")
-#' desc_data(airquality, format = "html", maxNA = 20)
+#' # desc_data(iris, format = "html")
+#' # desc_data(airquality, format = "html", maxNA = 20)
 
 
 
@@ -36,7 +36,7 @@ desc_data <- function(data,
 
   clss <- sapply(data, function(x) class(x)[length(class(x))])
 
-  lbl <- Hmisc::label(data)
+  lbl <- Hmisc::label(data,self = F)
 
   mm_lev <- unlist(lapply(data, function(x) {
     class_x <- class(x)[length(class(x))]
@@ -59,7 +59,7 @@ desc_data <- function(data,
 
   # Creacio data frame final
   df_res <- as.data.frame( cbind(nms,clss,lbl, mm_lev, compl_mis))
-  papeR::labels(df_res) <- c("Variable",
+  Hmisc::label(df_res, self = F) <- c("Variable",
                       "Type",
                       "Description",
                       "[Min,Max] or Levels",
