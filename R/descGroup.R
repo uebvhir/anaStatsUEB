6f320393650a6b01e55c09c3d65dae1864a62a6d
miriammota <mmota.foix@gmail.com>
2019-04-17 09:23:17 +0200

    millores

--- R/descGroup.R
+++ R/descGroup.R
@@ -1,29 +1,10 @@
-#' A descGroup Function
-#'
-#' DESCRIPCIO DE LA FUNCIO
-#' @param covariates a character string with names of variables.
-#' @param group factor variable. Outcome. Default value is NULL
-#' @param data data frame, list or environment (or object coercible by 'as.data.frame' to a data frame) containing the variables in the model. If they are not found in 'data', the variables are taken from 'environment(formula)'.
-#' @param method character string indicating the method to test use; possible values are 'param' or 'nonparam'. Default values is 'param'.
-#' @keywords summary ci qualitative descriptive exploratory
-#' @export descGroup
-#' @import kableExtra
-#' @examples
-#'  # set.seed(1)
-#'  # data <- df <- data.frame(MUT = factor(c(rep("A", 12),rep("B",13))),
-#'  #                          var = factor(sample(c("Yes", "no"), 25, replace = T)),
-#'  #                          size = rnorm(25),
-#'  #                          id = paste0("a",1:25))
-#'  # data$id <- as.character(data$id)
-#'  # descGroup(group  = "MUT",covariates = c("var","size","id"), data = data)
-
-
 descGroup <- function(covariates,
                       group = NULL,
                       data,
                       method = "non-param",
                       font_size = 11,
-                      width_lev = "8em", ...){
+                      width_lev = "8em",
+                      pval_cut = 0.05, ...){
 
 
   data <- data[,names(data) %in% c(covariates,group)]
@@ -51,23 +32,29 @@ descGroup <- function(covariates,
                     "Summary descriptives table",
                     paste0("Summary of results by groups of ", varname_group))
   # names(caption) <- c("Legend", ".")
-  # var <- sapply(strsplit(rownames(results), ".", fixed = T),"[[", 1)
+  var <- sapply(strsplit(rownames(results), ".", fixed = T),"[[", 1)
   align = rep("c",ncol(results))
   align[names(results) == "levels"] <- "l"
   # results <- results[,!names(results) %in% "variable"]
   # groups_row <- table(var)[unique(var)]
 
+  condition <- as.numeric(results$p.value) > pval_cut | is.na(as.numeric(results$p.value))
+  colorRow <- which(rownames(results) %in% grep(paste0(var[which(!condition)],collapse = "|"), rownames(results), value = T) )
   # groups_row <- cumsum(groups_row)
-  results_ht <- kable(results,escape = F, row.names = F,align = align,
-                      caption = caption)  %>%
+  results_ht <- results %>%
+    # mutate(p.value = cell_spec(p.value, "html", color = ifelse(condition,"black", "white"),
+    #                            background = ifelse(condition, "white", "#993489"))) %>%
+    kable(escape = F, row.names = F,align = align, caption = caption)  %>%
     kable_styling(latex_options = c("striped","hold_position", "repeat_header"),
                   font_size = font_size, full_width = F) %>%
     row_spec(0,background = "#993489", color = "white") %>%
     column_spec(2, width_max = width_lev) %>%
     column_spec(1, bold = T)  %>%
+    row_spec(colorRow, bold = F, color = "black",
+             background = "#ebe0e9") %>%
     add_footnote(footnote, escape = F,
-                notation = "symbol" ) #%>%
+                 notation = "symbol" ) #%>%
   # pack_rows(groups_row ,hline_after = F, indent = F)
 
-  return(list(group = group, covariates = covariates,pvalues = pvalues, results = results_ht))
+  return(list(group = group, covariates = covariates, selVar = unique(var[colorRow]), pvalues = pvalues, results = results_ht))
 }
