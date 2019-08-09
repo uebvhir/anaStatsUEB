052aa7a217c99bc6aa38bbaa12bd3ee38a8f8d21
miriammota <mmota.foix@gmail.com>
2019-08-09 12:07:38 +0200

    add params kable_ueb

--- R/kable_ueb.R
+++ R/kable_ueb.R
@@ -17,7 +17,7 @@
 kable_ueb <- function(tab,
                       font_size = 13,
                       full_width = FALSE, ...){
-  kable(tab) %>%
+  kable(tab,...) %>%
     kable_styling(latex_options = c("striped","hold_position", "repeat_header"),
                   font_size = font_size, full_width = full_width,...) %>%
     row_spec(0,background = "#993489", color = "white")
