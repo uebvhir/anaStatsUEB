486edcdfea6e5cdd6cd2361cbfa8d5ca98833ae9
miriammota <miriam.mota@vhir.org>
2019-11-05 14:23:09 +0100

    [kable_ueb]:millores variades

--- R/kable_ueb.R
+++ R/kable_ueb.R
@@ -5,6 +5,8 @@
 #' @param caption Character vector containing the table's caption or title. Set to NULL to suppress the caption. Default value is NULL.
 #' @param font_size A numeric input for table font size
 #' @param full_width A TRUE or FALSE variable controlling whether the HTML table should have 100% width. Since HTML and pdf have different flavors on the preferable format for full_width. If not specified, a HTML table will have full width by default but this option will be set to FALSE for a LaTeX table
+#' @param escape Boolean; whether to escape special characters when producing HTML or LaTeX tables. When escape = FALSE, you have to make sure that special characters will not trigger syntax errors in LaTeX or HTML.
+#' @param col.background A character string for row background color. Here please pay attention to the differences in color codes between HTML and LaTeX.
 #' @export kable_ueb
 #' @keywords kable html ueb
 #' @import kableExtra
@@ -21,11 +23,12 @@ kable_ueb <- function(tab,
                       font_size = 13,
                       full_width = FALSE,
                       escape = FALSE,
+                      col.background = "#993489",
                       ...){
 
   kable(tab, caption = caption, escape = escape, ...) %>%
     kable_styling(latex_options = c("striped","hold_position", "repeat_header"),
                   font_size = font_size, full_width = full_width) %>%
-    row_spec(0,background = "#993489", color = "white")
+    row_spec(0,background = col.background, color = "white")
 }
 
