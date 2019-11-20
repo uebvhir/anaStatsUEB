0d5877094c5c98aa3455875915b066467e467663
miriammota <miriam.mota@vhir.org>
2019-11-20 15:15:10 +0100

    [desc_group]: fixed table head. No funciona amb rmdformats (encara)

--- R/desc_group.R
+++ R/desc_group.R
@@ -137,7 +137,7 @@ desc_group <- function(covariates,
     #                            background = ifelse(condition, "white", "#993489"))) %>%
     kable(escape = F, row.names = F,align = align, caption = caption)  %>%
     kable_styling(latex_options = c("striped","hold_position", "repeat_header"),
-                  font_size = font_size, full_width = F) %>%
+                  font_size = font_size, full_width = F, fixed_thead = T) %>%
     row_spec(0,background = col.background, color = "white") %>%
     column_spec(which(names(results) == "levels"), width_max = width_lev) %>%
     column_spec(which(names(results) == "variable"), bold = T)   %>%
