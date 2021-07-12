802e81541e340df666c6bf372296c7eadbe68855
Mota Foix <39437789B@ir.vhebron.net>
2021-07-12 11:16:37 +0200

    new functions

---
+++ R/desc_changes.R
@@ -0,0 +1,32 @@
+#' desc_changes Function
+#'
+#' The function desc_changes is used to save messages of changes applied in database
+#' @param x   a string
+#' @export desc_changes
+#' @author Miriam Mota  \email{miriam.mota@@vhir.org}
+#' @examples
+#' desc_changes("")
+#'
+#' @return list_changes list with messages to report.
+#' var_sino <- c("Fum","alcohol")
+#' desc_changes(paste0("Los valores, 0 y 1 se han considerado 'No' y 'Si' respectivamente para las variables: ", paste(var_sino,collapse = ", ") ))
+#' desc_changes("Los valores superiores a 10000 para la variable n_il6_f han sido considerados NA ")
+#' list_changes
+#' #With results = 'asis' rmarkdown chunk
+#' for(i in seq_along(list_changes)){
+#' cat(list_changes[[i]])
+#' }
+
+#' @keywords message report changes
+
+
+desc_changes <- function(x){
+  x <- paste("\n * <span style='color:blue'>",x,"</span> \n")
+  if(!exists("list_changes")){
+    list_changes <<- list()
+    i = 0
+  }else{
+    i <- length(list_changes)
+  }
+  list_changes[[i+1]] <<- x
+}
---
+++ R/var_to_num.R
@@ -0,0 +1,32 @@
+#' var_to_num Function
+#'
+#' The function var_to_num is used to convert a character variable to numeric variable. Change ',' by '.'. Delete spaces. Return a numeric variable and a warning with the delete values.
+#' @param x   a character variable with numbers
+#' @export var_to_num
+#' @author Miriam Mota  \email{miriam.mota@@vhir.org}
+#' @examples
+#'
+#' @return a numeric variable and a warning with delete values.
+#'  edad <- c("6", "6,7", "10 ", "12.5", "3   ", "4,", "no se" )
+#'  Hmisc::label(edad) <- "Edad de los pacientes"
+#'  edad_num <- var_to_num(edad)
+#'  edad_num
+#'  Hmisc::label(edad_num)
+
+#' @keywords numeric character class change warnings
+
+
+var_to_num <- function(x){
+  lbl <- Hmisc::label(x)
+  x_orig <- x
+  x <- as.character(x)
+  x <- gsub(",",".", x)
+  x <- trimws(x)
+  x <- as.numeric(x)
+  na_value <- na.omit(unique(x_orig[is.na(x)]))
+  try(name_var <- unlist(strsplit(x,split = "$",fixed = T))[2], T)
+  if(length(na_value) >0)
+    warning(paste("Los valores", na_value, "de la variable '' han sido considerados datos faltantes"))
+  Hmisc::label(x) <- lbl
+  return(x)
+}
