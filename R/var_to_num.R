c672892eb430afdcf10f9b2e2e97f52f06954da6
Mota Foix <39437789B@ir.vhebron.net>
2025-02-13 10:44:58 +0100

    Update var_to_num.R
    retorna label en var_to_num

--- R/var_to_num.R
+++ R/var_to_num.R
@@ -25,6 +25,7 @@ var_to_num <- function( x ,pat = NULL, rep = NULL){
   orig <- x  #variable original
   x_new <-  as.numeric(trimws(reduce2(fixed(c(",", "%", pat)), fixed(c(".", "",rep)), .init = x, str_replace)))
   na_value <-   na.omit(unique(orig[is.na(x_new)]))
+  Hmisc::label(x_new) <- Hmisc::label(orig)
   if(length(na_value) >0){
     mss <- paste0("Los valores '", paste0(na_value, collapse = "', '"), "' de la variable '",txt_col,
                   "' han sido considerados datos faltantes")
